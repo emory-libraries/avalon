dadb43b8c3826d40c754fd8b4f6eded7828fe805
devanshu-m <devanshu_m@gwmail.gwu.edu>
2020-10-20 17:35:19 -0400

    Adds initial shib config

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_14_154529) do
+ActiveRecord::Schema.define(version: 2020_10_20_210257) do
 
   create_table "active_encode_encode_records", force: :cascade do |t|
     t.string "global_id"
